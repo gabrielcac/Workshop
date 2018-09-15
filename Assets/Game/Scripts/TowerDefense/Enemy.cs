b8b763446bca6b4d2601f7bb98f4634a4f420967
Gabriel Coutinho <gabrielcac@gmail.com>
2018-09-15 13:01:44 -0300

    Movimentando inimigos em direção ao portão

---
+++ Assets/Game/Scripts/TowerDefense/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Classe com os comportamentos de um inimigo.
+/// </summary>
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class Enemy : MonoBehaviour
+{
+	public Gate gate;
+
+	private float _maximumSpeed = 2;
+	private float _life = 100;
+	private NavMeshAgent _navMeshAgent;
+	private Animator _animator;
+
+	private void Awake()
+	{
+		_navMeshAgent = GetComponent<NavMeshAgent>();
+		_navMeshAgent.speed = _maximumSpeed;
+
+		_animator = GetComponent<Animator>();
+	}
+
+	private void OnEnable()
+	{
+		_navMeshAgent.Warp(transform.position);
+		_navMeshAgent.SetDestination(gate.transform.position);
+	}
+
+	private void Update()
+	{
+		_animator.SetFloat("Speed", _navMeshAgent.velocity.magnitude);
+	}
+
+	/// <summary>
+	/// Função chamada quando o inimigo é atingido por um disparo de uma das torres.
+	/// </summary>
+	/// <param name="damage"></param>
+	public void Damage(float damage)
+	{
+		_life -= damage;
+
+		// Se o dano é suficiente para matá-lo, toca a animação de morte
+		// Se não, toca a animação de dano.
+		if(_life <= 0)
+		{
+			_animator.SetTrigger("Die");
+			// Exercício: Usar Coroutine para destruir o corpo morto do inimigo após alguns segundos.
+		}
+		else
+		{
+			_animator.SetTrigger("Damage");
+			// Exercício: Usar Coroutine para parar a movimentação do inimigo por alguns segundos quando atingido.
+		}
+	}
+}
---
+++ Assets/Game/Scripts/TowerDefense/Gate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+	[SerializeField]
+	private float _life;
+
+	public void Damage(float damage)
+	{
+		_life = Mathf.Max(0, _life - damage);
+
+		Debug.Log("O protão sofreu " + damage + " e está com " + _life + " pontos de vida.");
+	}
+}
